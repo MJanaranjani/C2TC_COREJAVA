36b73f100770e8074003d3373d4748ae35debcbf
Udhayajana <Udhayajana@MSI>
2024-10-28 09:49:10 +0530

    COMMIT

---
+++ C2TC_JANARANJANI/src/com/tns/ifet/dayone/PrimitiveDataTypesDemo.java
@@ -0,0 +1,29 @@
+package com.tns.ifet.dayone;
+
+public class PrimitiveDataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println("Min range of byte is"+byteMin+"\n"+"max range of byte is"+byteMax);
+		short shortMax=32767;
+		short shortMin=-32768;
+		System.out.println("MinShort range of byte is"+shortMin);
+		System.out.println("MaxShort range of byte is"+shortMax);
+		int maxInt=2147483647;
+		int minInt=-2147483647;
+		System.out.println("MinInt range of byte is"+minInt+"\n"+"MaxInt range of byte is"+maxInt);
+		long maxLong=9223372036854775807L;
+		long minLong =-9223372036854775808L;
+		System.out.println("Minlong range of bytes is"+minLong+"\n"+"Maxlong range of byte is"+maxLong);
+		float f=3234.141243278345f;
+		double d=3456.14124512345678902345678914f;
+		System.out.println("float value is"+f+"\n"+"double value is"+d);
+		boolean flag=false;
+		System.out.println("boolean value is"+flag);
+		
+
+	}
+
+}
